f95f5435357d7e876444dba2c87616d4727f780f
fsantosandrade <160565775+fsantosandrade@users.noreply.github.com>
2024-04-25 01:40:31 -0300

    configuração básica dos botões setas

--- assets/js/main.js
+++ assets/js/main.js
@@ -21,11 +21,13 @@ function addEventListenerToCards(){
 
 document.addEventListener("DOMContentLoaded", addEventListenerToCards)
 
+const carrosel = document.querySelector('.carrosel')
+const transform = carrosel.style.transform
+const rotate = transform.match(/rotateY\((-?\d+deg)\)/i)
+
 function selectCarroselItem(selected) {
     const selectedItem = selected.id
-    const carrosel = document.querySelector('.carrosel')
-    const transform = carrosel.style.transform
-    const rotate = transform.match(/rotateY\((-?\d+deg)\)/i)
+
     const rotacao = -120 * (Number(selectedItem) - 1)
     const newTransform = transform.replace(rotate[0], `rotateY(${rotacao}deg)`)
 
@@ -35,4 +37,37 @@ function selectCarroselItem(selected) {
     buttonActive.classList.remove('active')
 
     selected.classList.add('active')
+}
+
+
+const arrow2 = document.querySelector('.arrow-right')
+const arrow1 = document.querySelector('.arrow-left')
+
+
+function clickArrowLeft(selected) {
+    const selectedArrow = selected.id
+    let arrowValue = Number(selectedArrow) - 2
+    const rotacao = 120 * arrowValue
+    const newTransform = transform.replace(rotate[0], `rotateY(${rotacao}deg)`)
+    
+    carrosel.style.transform = newTransform
+    
+    arrow1.id = ''
+    arrow1.id = Number(selectedArrow) + 1
+    
+    arrow2.id -= 1
+}
+
+function clickArrowRight(selected) {
+    const selectedArrow = selected.id
+    let arrowValue = Number(selectedArrow) - 3
+    const rotacao = -120 * arrowValue
+    const newTransform = transform.replace(rotate[0], `rotateY(${rotacao}deg)`)
+    
+    carrosel.style.transform = newTransform
+    
+    arrow2.id = ''
+    arrow2.id = Number(selectedArrow) + 1
+
+    arrow1.id -= 1
 }
